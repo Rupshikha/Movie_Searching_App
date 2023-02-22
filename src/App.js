b58ffdcc071b25be85c759f0f0ff8a3d35dcbfa3
Rupshikha28 <rupshikha@techvariable.com>
2023-02-22 16:10:13 +0530

    delete Api folder

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import './App.css';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import MovieApi from './Apis/MovieApi';
-import Header from './components/Header/Header';
-import Home from './components/Home/Home';
-import MovieDetail from './components/MovieDetail/MovieDetail';
-import FavMovieList from './components/FavMovie/FavMovieList';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import FavMovieList from "./components/FavMovie/FavMovieList";
 
 function App() {
   return (
     <div>
       <Router>
-           <Header/>
-           <Routes>
-              <Route path="/" exact element={<Home/>}/>
-              <Route path="/movie/:imdbID" element={<MovieDetail/>}/>
-              <Route path="/favmovie" element={<FavMovieList/>}/>
-          </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<Home />} />
+          <Route path="/movie/:imdbID" element={<MovieDetail />} />
+          <Route path="/favmovie" element={<FavMovieList />} />
+        </Routes>
       </Router>
     </div>
   );
